8a40eb3e0b1c414cf001d4ef4820aecc7a70bf2e
Anubhav Gupta <82750157+Anubhav6205@users.noreply.github.com>
2022-04-02 00:08:08 +0530

    Add files via upload

---
+++ sort_an_array_0s_1s_2s.cpp
@@ -0,0 +1,12 @@
+
+/*Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.*/
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // coode here 
+        sort(a,a+n);
+    }
+    
+};
