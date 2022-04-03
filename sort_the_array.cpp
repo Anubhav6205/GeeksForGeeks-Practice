c4610287d8c84ad1a9f88511e89b2cb7fa7a944d
Anubhav Gupta <82750157+Anubhav6205@users.noreply.github.com>
2022-04-03 21:54:42 +0530

    Add files via upload

---
+++ sort_the_array.cpp
@@ -0,0 +1,9 @@
+//Given a random set of numbers, Print them in sorted order.
+class Solution{
+    public:
+    vector<int> sortArr(vector<int>arr, int n){
+        sort(arr.begin(),arr.end());
+       return arr;
+    //complete the function here
+    }
+};
